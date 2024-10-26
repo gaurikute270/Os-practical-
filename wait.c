ff058257cea92cb6dd3adf41d324f7509babe2bd
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-26 18:10:28 +0530

    Update

---
+++ wait.c
@@ -0,0 +1,20 @@
+ #include"stdio.h"
+ #include"sys/types.h"
+ #include"unistd.h"
+ #include"sys/wait.h"
+ int main()
+{
+pid_t pid fork();
+if (pid==0)
+printf( "-> Child process\n");
+ else
+{
+printf("-> Parent process\n");
+wait(NULL);
+
+// this statement will always be executed at last due to wait() 
+printf(" -> child process terminated\n");
+}
+printf("->End\n");
+return 0;
+}
